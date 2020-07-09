7623b0829c23438daae4141ac55444cdb7f7a7f2
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2020-07-09 17:17:37 -0600

    SkeletonFiles
    This is our first mission.

---
+++ Poll.java
@@ -0,0 +1,39 @@
+public class Poll {
+	private String name = "Unnamed Poll";
+	private Party[] parties;
+	private int numberOfParties = 0;
+
+	public Poll(String name, int maxNumberOfParties) {
+		this.name = name; 
+		parties = new Party[maxNumberOfParties];
+	}
+	
+	public String getPollName() {
+		return name;
+	}
+	
+	public int getNumberOfParties() {
+		return numberOfParties;
+	}
+	
+	public void addParty(Party aParty) {
+		parties[numberOfParties++] = aParty;
+	}
+
+	public Party getParty(String name) {
+		return null;
+	}
+	
+	public Party[] getPartiesSortedBySeats() {
+		return parties;
+	}
+
+	public Party[] getPartiesSortedByVotes() {
+		return parties;
+	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}
+}
