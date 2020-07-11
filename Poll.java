6945ed04adc4d085f3ea7b50a815893ad15d2b1a
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2020-07-11 12:55:34 -0600

    Update Poll.java
    I need help to addParty method.

--- Poll.java
+++ Poll.java
@@ -4,26 +4,51 @@ public class Poll {
 	private int numberOfParties = 0;
 
 	public Poll(String name, int maxNumberOfParties) {
-		this.name = name; 
-		parties = new Party[maxNumberOfParties];
+		this.name = name;
+		if(maxNumberOfParties >= 1){
+			parties = new Party[maxNumberOfParties];
+		}else {
+			parties = new Party[10];
+		}
+
+
 	}
-	
+
 	public String getPollName() {
 		return name;
 	}
-	
+
 	public int getNumberOfParties() {
 		return numberOfParties;
 	}
-	
+
 	public void addParty(Party aParty) {
-		parties[numberOfParties++] = aParty;
+		if(aParty == null)
+		System.out.print("The enter message is empty.");
+		for(Party party: parties) {
+			if (party.getName().equalsIgnoreCase(aParty.getName())){
+				party = aParty;
+			}
+		}
+		if(numberOfParties < parties.length) {
+			parties[numberOfParties] = aParty;
+			numberOfParties++;
+		}else {
+			System.out.println("No room.");
+		}
 	}
 
+
+
 	public Party getParty(String name) {
+		for(Party party:parties) {
+		if(party.getName().equals(name)) {
+			return party;
+			}
+		}
 		return null;
 	}
-	
+
 	public Party[] getPartiesSortedBySeats() {
 		return parties;
 	}
@@ -31,9 +56,14 @@ public class Poll {
 	public Party[] getPartiesSortedByVotes() {
 		return parties;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "";
+		String result = name +"\n";
+		for(Party aParty: parties) {
+			if(aParty!= null)
+			result += (aParty + "\n");
+		}
+		return result;
 	}
 }
