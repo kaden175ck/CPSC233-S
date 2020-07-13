cb344f481984faf8865561451c55c577b8575319
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2020-07-13 12:59:00 -0600

    Update PollList.java
    This is PollList class(unfinished) so far.

--- PollList.java
+++ PollList.java
@@ -3,8 +3,21 @@ public class PollList {
 	private int numOfSeats;
 	
 	public PollList(int numOfPolls, int numOfSeats) {
+		if (numOfPolls >= 1) {
 		polls = new Poll[numOfPolls];
+		}
+		else {
+			polls = new Poll[5];
+		
+		}
+		if (numOfSeats >=1) {
 		this.numOfSeats = numOfSeats;
+		}
+		
+		else {
+			this.numOfSeats = 10;
+		}
+		
 	}
 	
 	public int getNumOfSeats() {
@@ -16,10 +29,29 @@ public class PollList {
 	}
 
 	public void addPoll(Poll aPoll) {
-		int index = 0;
-		for (; index < polls.length && polls[index] != null; index++) {	
+		if (aPoll != null) {
+		boolean already = false;
+		int full = 0;
+		// testing to see if the poll already exists in the list and if the list is full 
+		for (int index = 0; index < polls.length && polls[index] != null; index++) {	
+			if (polls[index] == aPoll) {
+				already = true;
+			}
+			full++;
 		}
-		polls[index] = aPoll;
+		//if the poll doesn't exist and the list is not full, add the poll to the end of the list
+		if (already == false && full < polls.length) {
+			polls[full] = aPoll;
+		}
+		// if the list is full, print error message saying the list is full
+		else if (full == polls.length) {
+			System.out.println("List is full, no further polls can be added.");
+		}
+	}
+		// if aPoll is empty, print error message and do not change Poll list
+		else {
+			System.out.println("Poll empty, no changes will be made.");
+		}
 	}
 	
 	public Poll getAggregatePoll(String[] partyNames) {
@@ -27,15 +59,29 @@ public class PollList {
 	}
 	
 	public Party getAveragePartyData(String partyName) {
-		return null;
+		Party partyName = new Party();
+		//expected number of seats
+		
+		int numberSeats = 0;
+		double percentageOfVotesTotal = 0;
+		for (int index = 0;index < polls.length ;index++) {
+			int projectedNumberSeats = partyName.getProjectedNumberSeats();
+			numberSeats = numberSeats + projectedNumberSeats;
+			double percentageOfVotes = partyName.getProjectedPercentageOfVotes();
+			percentageOfVotesTotal = percentageOfVotesTotal + percentageOfVotes;
+		}
+		double avgNumberSeats = (double)numberSeats/polls.length;
+		double avgPercentageOfVotes = percentageOfVotesTotal/polls.length;
+		return partyName;
 	}
 	
 	public Poll adjustPollToMaximums(Poll aPoll) {
 		return aPoll;
 	}
 	
+	
 	@Override
 	public String toString() {
-		return "";
+		return "Number of seats: " + numOfSeats;
 	}
 }
