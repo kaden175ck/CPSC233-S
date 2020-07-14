043e606d86641ffb3dd474b9eef551ea6c1e4899
kennethl778 <67394621+kennethl778@users.noreply.github.com>
2020-07-14 16:37:04 -0600

    Update PollList.java
    not complete

--- PollList.java
+++ PollList.java
@@ -1,85 +1,132 @@
 public class PollList {
 	private Poll[] polls;
 	private int numOfSeats;
-	
+
 	public PollList(int numOfPolls, int numOfSeats) {
 		if (numOfPolls >= 1) {
-		polls = new Poll[numOfPolls];
+			polls = new Poll[numOfPolls];
 		}
 		else {
 			polls = new Poll[5];
-		
+
 		}
 		if (numOfSeats >=1) {
-		this.numOfSeats = numOfSeats;
+			this.numOfSeats = numOfSeats;
 		}
-		
+
 		else {
 			this.numOfSeats = 10;
 		}
-		
+
 	}
-	
+
 	public int getNumOfSeats() {
 		return numOfSeats;
 	}
-	
+
 	public Poll[] getPolls() {
 		return polls;
 	}
 
 	public void addPoll(Poll aPoll) {
+		System.out.println("aPoll   "+aPoll);
 		if (aPoll != null) {
-		boolean already = false;
-		int full = 0;
-		// testing to see if the poll already exists in the list and if the list is full 
-		for (int index = 0; index < polls.length && polls[index] != null; index++) {	
-			if (polls[index] == aPoll) {
-				already = true;
+			boolean already = false;
+			int full = 0;
+			// testing to see if the poll already exists in the list and if the list is full
+			for (int index = 0; index < polls.length && polls[index] != null && already != true; index++) {
+				String pollName = polls[index].getPollName();
+				System.out.println("poll name    "+pollName);
+				if (pollName.equalsIgnoreCase(aPoll.getPollName())) {
+					already = true;
+				}
+				full++;
 			}
-			full++;
+			System.out.println("already     " + already);
+			//if the poll doesn't exist and the list is not full, add the poll to the end of the list
+			if (already == true) {
+				polls[full-1] = aPoll;
+			}
+			else {
+
+				if (full < polls.length) {
+					// if the list is not full add the new poll to the end of the list
+					polls[full] = aPoll;
+
+				}
+				// if the list is full, print error message saying the list is full
+				else if (full == polls.length) {
+					//if the list is full and no duplicate it present, print an error message
+					System.out.println("List is full, no further polls can be added.");
+
+				}
+
+			}
 		}
-		//if the poll doesn't exist and the list is not full, add the poll to the end of the list
-		if (already == false && full < polls.length) {
-			polls[full] = aPoll;
-		}
-		// if the list is full, print error message saying the list is full
-		else if (full == polls.length) {
-			System.out.println("List is full, no further polls can be added.");
-		}
-	}
 		// if aPoll is empty, print error message and do not change Poll list
-		else {
+		else
 			System.out.println("Poll empty, no changes will be made.");
+
+	}
+
+
+
+	public Poll getAggregatePoll(String[] partyNames){
+		//make a new aggregate poll named "Aggregate"
+		Poll Aggregate = new Poll("Aggregate",partyNames.length);
+		for (int index = 0; index < partyNames.length; index++) {
+			Aggregate.addParty(getAveragePartyData(partyNames[index]));
+			String nameOfParty = partyNames[index];
+			if (numOfSeats != "total actual number of seats") {
+				//then adjust the numbers accordingly
+				float correctedNumOfSeats = Aggregate[index].getProjectedNumberOfSeats() * ((float)numOfSeats/total actual number of seats);
+				Aggregate[index].setProjectedNumberOfSeats(correctedNumOfSeats);
+			}
+			if (total percent of votes != 100) {
+
+			}
+
 		}
+
+		return Aggregate;
 	}
-	
-	public Poll getAggregatePoll(String[] partyNames) {
-		return polls[0];
-	}
-	
+
 	public Party getAveragePartyData(String partyName) {
-		Party partyName = new Party();
+		Party party = new Party(partyName);
 		//expected number of seats
-		
+
 		int numberSeats = 0;
 		double percentageOfVotesTotal = 0;
+		int length = 0;
 		for (int index = 0;index < polls.length ;index++) {
-			int projectedNumberSeats = partyName.getProjectedNumberSeats();
-			numberSeats = numberSeats + projectedNumberSeats;
-			double percentageOfVotes = partyName.getProjectedPercentageOfVotes();
-			percentageOfVotesTotal = percentageOfVotesTotal + percentageOfVotes;
+			//inside polls getParty from party class --> argument party name
+			// how to get the projected number of seats to the party created
+			Party partyObject = polls[index].getParty(partyName);
+			if (partyObject != null) {
+				int projectedNumberSeats = (int)(partyObject.getProjectedNumberOfSeats());
+				numberSeats = numberSeats + projectedNumberSeats;
+				double percentageOfVotes = partyObject.getProjectedPercentageOfVotes();
+				percentageOfVotesTotal = percentageOfVotesTotal + percentageOfVotes;
+				length++;
+
+			}
 		}
-		double avgNumberSeats = (double)numberSeats/polls.length;
-		double avgPercentageOfVotes = percentageOfVotesTotal/polls.length;
-		return partyName;
+
+		float avgNumberSeats = (float)numberSeats/length;
+		float avgPercentageOfVotes = (float)percentageOfVotesTotal/length;
+
+
+		// create new party object
+		party.setProjectedNumberOfSeats(avgNumberSeats);
+		party.setProjectedPercentageOfVotes(avgPercentageOfVotes);
+		return party;
 	}
-	
+
 	public Poll adjustPollToMaximums(Poll aPoll) {
 		return aPoll;
 	}
-	
-	
+
+
 	@Override
 	public String toString() {
 		return "Number of seats: " + numOfSeats;
