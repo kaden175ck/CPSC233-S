fed9e72149aa8850c5d5506ca6f0a3409c247f29
jungsukim2609 <67721356+jungsukim2609@users.noreply.github.com>
2020-07-16 00:20:55 -0600

    Update TextApplication.java

--- TextApplication.java
+++ TextApplication.java
@@ -1,26 +1,56 @@
+/**
+ * @author Jungsu Kim
+ * @Classname: TextApplication
+ * @Description: This class gets other class "Party", "Poll", "PollList", and "Factory" and implements them together
+ */
 import java.util.Scanner;
 
 public class TextApplication {
 	public static final int MAX_NUMBER_OF_STARS = 25;
 	private PollList polls;
 	
+	/**
+	 * Title: TextApplication
+	 * Description: This a default constructor of the class 
+	 */
 	public TextApplication() {
 	}
 	
+	/**
+	 * Title: TextApplication
+	 * Description: Another constructor which initializes the PollList object "Polls"
+	 * @param polls
+	 */
 	public TextApplication(PollList polls) {
 		this.polls = polls;
 	}
 	
+	/**
+	 * Title: displayPollBySeat
+	 * Description: This displays each poll and the parties included, they are arranged by seats
+	 * @param partyNames
+	 */
 	public void displayPollsBySeat(String[] partyNames) {
 		for(Poll a : polls.getPolls()) {
 			displayPollDataBySeat(a);
 		}
 	}
 	
+	/**
+	 * Title: PollList getPolls
+	 * Getter method
+	 * Description: Gets the PollList object "Polls"
+	 * @return polls
+	 */
 	public PollList getPolls() {
 		return polls;
 	}
 
+	/**
+	 * Title: displayPollDataBySeat
+	 * Description: Takes in poll as an argument and displays each party and arranges them by seat
+	 * @param aPoll
+	 */
 	public void displayPollDataBySeat(Poll aPoll) {
 		int starsNeededForMajority = MAX_NUMBER_OF_STARS/2 + 1;
 		System.out.println(aPoll.getPollName());
@@ -30,56 +60,67 @@ public class TextApplication {
 		System.out.println("\n");
 	}
 	
+	/**
+	 * Title: Run
+	 * Description: This body of code is to initializes all the methods made above and creates an application allowing user input and output
+	 */
 	public void run() {
-Scanner userinp = new Scanner(System.in);
+		Scanner userInput = new Scanner(System.in);
 		
-		System.out.println("Welcome to the poll tracker");
-		System.out.print  ("How many seats are available in the election? ");
-		int     numSeats = userinp.nextInt();
-		Factory factory  = new Factory(numSeats);
-		userinp.nextLine();
+		System.out.println("Welcome to the poll tracker!");
+		System.out.println("How many seats are available in the election? ");
+		int seats = userInput.nextInt();
+		Factory factory  = new Factory(seats);
+		userInput.nextLine();
 		
-		System.out.println("\nWhich parties are in the election (provide names, comma separated): ");
-		String[] partyNames = userinp.nextLine().split(",");
-		factory.setPartyNames(partyNames);
+		System.out.println("\nWhich parties are in the election (provide names, separate by comma): ");
+		String[] parties = userInput.nextLine().split(",");
+		factory.setPartyNames(parties);
 		
-		System.out.print  ("How many polls do you want to track with this application? ");
-		int numPolls = userinp.nextInt();
-		userinp.nextLine();
+		System.out.print  ("\nHow many polls do you want to track with this application? ");
+		int numPolls = userInput.nextInt();
+		userInput.nextLine();
 		
 		System.out.print  ("\nWould you like to create a random set of polls? [y/n] ");
-		String isRandom = userinp.nextLine();
-		/**
-		 * Factory handles both random and prompt-based generation of the poll list
-		 */
-		if (isRandom.equals("y")) {
-			polls = new PollList(numPolls, numSeats);
-			for (int i = 0; i < numPolls; i++) {	
-				polls.addPoll(factory.createRandomPoll("Poll " + i));
+		String randomPoll = userInput.nextLine();
+
+		if (randomPoll == ("y")) {
+			polls = new PollList(numPolls, seats);
+			int counter = 0;
+			while (counter < numPolls) {	
+				polls.addPoll(factory.createRandomPoll("Poll " + counter));
+				counter ++;
 			}
 		}
 		else {
 			polls = factory.promptForPollList(numPolls);
 		}
 		
-		String userOption;
-		do {
+		String option = "";
+		int counter = 0;
+		while(counter == 0) {
 			System.out.println("Options: all (show result of all polls), aggregate (show aggregate result), quit (end application)");
-			System.out.print("Choose an option: ");
-			userOption = userinp.nextLine();
-			if (userOption.equals("all")) {
-				displayPollsBySeat(partyNames);
+			System.out.println("Choose an option: ");
+			option = userInput.nextLine();
+			if (option.equals("all")) {
+				displayPollsBySeat(parties);
 			}
-			else if (userOption.equals("aggregate")) {
+			if (option.equals("aggregate")) {
 				System.out.println("Aggregate");
-				displayPollDataBySeat(polls.getAggregatePoll(partyNames));
+				displayPollDataBySeat(polls.getAggregatePoll(parties));
 			}
-		} while (userOption.equals("quit") == false);
-		
-		userinp.close();
+			if (option.equals("quit")) {
+				counter ++;
+			}
+		}
+		userInput.close();
 	}
 	
-	
+	/**
+	 * Title: main
+	 * Description: runs the java code and initializes the TextApplication class 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		TextApplication app = new TextApplication(null);
 		app.run();
