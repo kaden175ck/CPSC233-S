eb6ac099112269abbdc8a388a37c121dd29f7218
jungsukim2609 <67721356+jungsukim2609@users.noreply.github.com>
2020-07-21 05:27:01 -0600

    Create Game.java

---
+++ Final project text based application/Game.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Game {
+	private HumanPlayer human;
+	private static GameConfiguration config;
+	private AI computer; //AI class
+	private static Scanner input = new Scanner(System.in);
+	private int gameMode;
+
+	public void setup() {
+		
+	}
+
+	public void game() {
+		System.out.println("Welcome to Gomoku");
+		System.out.println("Please choose between following options:");
+		System.out.println("1: Player vs AI, 2: Player vs Player");
+		gameMode = input.nextInt();
+		//if gamemde =human vs human  
+		//private humanplayer human2
+		
+		System.out.println("Which colour would you like to be (player1):")
+		System.out.println("1: Black, 2: White");
+		int colour = input.nextInt();
+		//human.setcolor(colour);
+		
+		while (config.hasWon() == false && gameMode == 1) {
+			if (human.getColor() == black) {
+				human.play();
+				human2.play();
+			}
+			else {
+				human2.play();
+				human.play();
+			}
+		}
+		while (config.hasWon() == false && gameMode == 2) {
+			if (human.getColor == black) {
+				human.play();
+				computer.play();
+			}
+			else {
+				computer.play();
+				human.play();
+			}
+		}
+		
+	}
+	
+	public void main(String[] args) {
+		game();
+	}
+	
+	
+	
+}
