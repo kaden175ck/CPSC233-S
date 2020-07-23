aa426713e3a68ed0e972c77ed3626f9125e83c7c
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2020-07-22 23:50:54 -0600

    Update Game.java
    Game view untile now

--- Final project text based application/Game.java
+++ Final project text based application/Game.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
 
-public class Game {
-	
-	private Scanner input = new Scanner(System.in);
-	
+public class Game {	
 	public static void main(String[] args) {
 		//Draw board
 		Board aBoard = new Board();
 		aBoard.creatBoard();
+		
 
+		HumanPlayer aHuman1 = new HumanPlayer();
+		aHuman1.setColor();
+		
+		HumanPlayer aHuman2 = new HumanPlayer();
+		aHuman2.setColor();
+		
+		aHuman1.play(aBoard);
+		
+		aHuman2.play(aBoard);
 		
 		
 	}
