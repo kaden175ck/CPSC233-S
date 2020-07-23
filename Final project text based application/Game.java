8a5fdd54b049a3810f8aff4ef9062f3f4dfc7a13
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2020-07-23 01:23:08 -0600

    Update Game.java
    worked view

--- Final project text based application/Game.java
+++ Final project text based application/Game.java
@@ -7,19 +7,36 @@ public class Game extends GameConfiguration {
 		
 
 		HumanPlayer aHuman1 = new HumanPlayer();
-		aHuman1.setColor();
+		int human1Color = aHuman1.setColor();
 		
-		//HumanPlayer aHuman2 = new HumanPlayer();
-		//aHuman2.setColor();
+		HumanPlayer aHuman2 = new HumanPlayer();
+		aHuman2.setOpponentColor(human1Color);
 		
 		 int counter = 0;
-		
 		 while((counter<=112)) {
 			 aHuman1.play(aBoard);
 			 
 			 if(checkWon(aHuman1.getRow(), aHuman1.getCol(), aBoard) == true) {
-				System.out.println("You win!");
+				 if(human1Color == 1) {
+					 System.out.println("Black win!");
+				 }
+				 else if(human1Color == 2) {
+					 System.out.println("White win!");
+				 }
+				break;
 				}
+			 
+			 aHuman2.play(aBoard);
+			 if(checkWon(aHuman2.getRow(), aHuman2.getCol(), aBoard) == true) {
+				 if(human1Color == 1) {
+					 System.out.println("White win!");
+				 }
+				 else if(human1Color == 2) {
+					 System.out.println("Black win!");
+				 }
+				break;
+				}
+			 
 			 
 			 counter++;	
 		}
