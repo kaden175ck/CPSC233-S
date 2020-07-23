4060725d4128d945f064d14df21bc4ec6954a39d
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2020-07-22 23:51:09 -0600

    Update HumanPlayer.java
    HumanPlayer view untile now

--- Final project text based application/HumanPlayer.java
+++ Final project text based application/HumanPlayer.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class HumanPlayer{
 	
-	private char token; //will implement later 
+	private int token; //will implement later 
 	private Scanner input = new Scanner(System.in);
 	//private char board[][] = new char[15][15];//will probably get rid of this, we need to make a board class
 	
@@ -16,32 +16,40 @@ public class HumanPlayer{
 	private int userColor;
 	
 	/**
-	 * Title: getRow
+	 * Title: setRow
 	 * Description: Prompts user for row 
 	 * @return value of row
 	 */
-	public int getRow() {
+	public int setRow() {
 		System.out.println("Enter row where you want piece to go: ");
 		row = input.nextInt();
 		return row;
 	}
 	/**
-	 * Title: getCol
+	 * Title: setCol
 	 * Description: Prompts user for column 
 	 * @return value of column
 	 */
-	public int getCol() {
+	public int setCol() {
 		System.out.println("Enter column where you want piece to go: ");
 		col = input.nextInt();
 		return col;
 	}
 	
-	public int getColor() {
+	public int setColor() {
 		System.out.println("Pick the color of your pieces. 1: Black, 2: White");
 		color = input.nextInt();
 		if(color ==1 || color == 2)
 			userColor = color;
+		else {
+			System.out.println("Please select valid color. (1: White, 2: Black)");
+		}
 		return userColor;
+	}
+	
+	public void setOpponentColor(int aColor) {
+		if (aColor == 1) userColor = 2;
+		if (aColor == 2) userColor = 1;
 	}
 	
 
@@ -54,35 +62,44 @@ public class HumanPlayer{
 	 */
 	public void placeToken(Board aBoard) {
 		try { 								//try statement for placing piece
-			char value = aBoard.getValue(row, col);
+			int value = aBoard.getValue(row, col);
 			if(value == 0) {
 			userCol = col;
 			userRow = row;
 			
 			value = token; 		//import from token class
 			input.close();
-			System.out.println("Putting piece at coordinates: (" + userRow +"," + userCol +")");
 			}
 			else {							//if place in board is occupied, show error
 				System.out.println("Invalid move: coordinates already occupied");
-				getRow();
-				getCol();
+				setRow();
+				setCol();
 				placeToken(aBoard);
 			}
 		} 
 		catch(ArrayIndexOutOfBoundsException E) { //if user input is beyond board, prompt again
 			System.out.println("invalid input, enter a number between 0 and 15");
-			getRow();
-			getCol();
+			setRow();
+			setCol();
 			placeToken(aBoard);
 			}
 		}
 	
 	
 	public void play(Board aBoard) { //this method we will call when it's players turn
-		getRow();
-		getCol();
+		setRow();
+		setCol();
 		placeToken(aBoard);
+		aBoard.setValue(userRow, userCol, userColor);
+		for(int i=0;i<15;i++) {
+			System.out.println();	
+			for(int j=0;j<15;j++) {
+				System.out.print(aBoard.getValue(i, j) + " ");
+			}
+		}
+		System.out.println();
+		System.out.println();
+	
 	}
 	
 	
