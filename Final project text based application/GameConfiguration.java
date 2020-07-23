c471c70adb4f26e8b96ad3131363866d7106c934
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2020-07-23 00:47:18 -0600

    Update GameConfiguration.java
    1

--- Final project text based application/GameConfiguration.java
+++ Final project text based application/GameConfiguration.java
@@ -1,62 +1,38 @@
-package application;
 
-public class GameConfiguration extends Tile {
-	protected static Tile[] [] board = new Tile[15] [15];
-	
-	public int getTileRowIndex() {
+public class GameConfiguration{
+
+	public int humanVSAI() {
 		return 0;
 	}
 	
-	public int getTilecolumnIndex() {
+	public int humanVShuman(HumanPlayer human1, HumanPlayer human2) {
 		return 0;
 	}
 	
-	public static void Board() {
-		System.out.println(" Gomoku");// Prompt to user game start.
-		for(int i=0;i<15;i++) {
-			System.out.println();
-			for(int j=0;j<15;j++) {
-				System.out.print(board[i][j] + "0");
-			}
-		}
-	}
 	
-	public boolean isValidTile(Tile aTile) {
-		boolean tileVaild = false;
-		//the xcoordinate and ycoordinate of 'tile' is the same as its index of array 'board'. 
-		if(0<=aTile.getxCoord() && aTile.getxCoord()<=15 && 0<=aTile.getyCoord() && aTile.getyCoord()<=15 && aTile != null) {
-			tileVaild = true;
-		}
-		return tileVaild;
-	}
-	
-	public boolean hasWon(Tile aTile) {
-		return (checkDiagonalDownLeftToTopRight(aTile) || checkDiagonalTopLeftToDownRight(aTile) || checkRow(aTile) || checkCol(aTile)); 
+	public static boolean checkWon(int row,int col, Board aBoard) {
+		return (checkDiagonalDownLeftToTopRight(row,col,aBoard) || checkDiagonalTopLeftToDownRight(row,col,aBoard) || checkRow(row,col,aBoard) || checkCol(row,col,aBoard)); 
 	}
 	
 	//
-    public boolean checkRow(Tile aTile){
+    public static boolean checkRow(int row, int col, Board aBoard){
 
         //identify which color
-        int color = aTile.getColor();
-       //the xcoordinate and ycoordinate of 'tile' is the same as its index of array 'board'. 
-        int xIndex = aTile.getxCoord();
-        int yIndex = aTile.getyCoord();
+        int color = aBoard.getValue(row, col);
         //count how many tiles in row
         int count=1;
         // check result
         boolean checkResult = false;
         
         //row right
-        for(int i=1;(i<5) && (color==board[yIndex][xIndex+i].getColor() && board[yIndex][xIndex+i]!=null);i++) {
-            if((xIndex+i)<=15) {
+        for(int i=1;(i<5) && (color==aBoard.getValue(row, col+i));i++) {
+            if((col+i)<=15) {
                 count++;
             }
         }
         //row left
-        
-        for(int i=1;(i<5) && (color==board[yIndex][xIndex-i].getColor() && board[yIndex][xIndex+i]!=null);i++) {
-            if((xIndex-i)>=0) {
+        for(int i=1;(i<5) && (color==aBoard.getValue(row, col-i));i++) {
+            if((col-i)>=0) {
                 count++;
             }
         }
@@ -66,26 +42,24 @@ public class GameConfiguration extends Tile {
     	return checkResult;
     }
        
-     public boolean checkCol(Tile aTile){
+     public static boolean checkCol(int row, int col, Board aBoard){
     	 //identify which color
-         int color = aTile.getColor();
+         int color = aBoard.getValue(row, col);
         //the xcoordinate and ycoordinate of tiles is the same as the index of their array index. 
-         int xIndex = aTile.getxCoord();
-         int yIndex = aTile.getyCoord();
          //count how many tiles in row
          int count=1;
          // check result
          boolean checkResult = false;
             
          //column down
-         for(int i=1;(i<5) && (color==board[yIndex+i][xIndex].getColor() && board[yIndex+i][xIndex]!=null);i++) {
-             if((yIndex+i)<=15) {
+         for(int i=1;(i<5) && (color==aBoard.getValue(row+i, col));i++) {
+        	 if((row+i)<=15) {
                  count++;
              }
          }
         //column up
-         for(int i=1;(i<5) && (color==board[yIndex-i][xIndex].getColor() && board[yIndex-i][xIndex]!=null);i++) {
-             if((yIndex-i)>=0) {
+         for(int i=1;(i<5) && (color==aBoard.getValue(row-i, col));i++) {
+             if((row-i)>=0) {
                  count++;
              }
          }
@@ -95,26 +69,24 @@ public class GameConfiguration extends Tile {
          return checkResult;
      }
          
-    public boolean checkDiagonalDownLeftToTopRight(Tile aTile) {
+    public static boolean checkDiagonalDownLeftToTopRight(int row, int col, Board aBoard) {
     	 //identify which color
-         int color = aTile.getColor();
+         int color = aBoard.getValue(row, col);
         //the xcoordinate and ycoordinate of tiles is the same as the index of their array index. 
-         int xIndex = aTile.getxCoord();
-         int yIndex = aTile.getyCoord();
          //count how many tiles in row
          int count=1;
          // check result
          boolean checkResult = false;
          
          //upright
-         for(int i=1;(i<5) && (color==board[yIndex-i][xIndex+i].getColor() && board[yIndex-i][xIndex+i]!=null);i++) {
-             if(((yIndex-i)>=0) && (xIndex+i<=15)) {
+         for(int i=1;(i<5) && (color==aBoard.getValue(row-i, col+1));i++) {
+             if(((row-i)>=0) && (col+i<=15)) {
                  count++;
              }
          }
          //downleft
-         for(int i=1;(i<5) && (color==board[yIndex+i][xIndex-i].getColor() && board[yIndex+i][xIndex-i]!=null);i++) {
-             if(((yIndex+i)<=15) && (xIndex-i>=0)) {
+         for(int i=1;(i<5) && (color==aBoard.getValue(row+i, col-1));i++) {
+             if(((row+i)<=15) && (row-i>=0)) {
                   count++;
              } 
          }
@@ -124,26 +96,24 @@ public class GameConfiguration extends Tile {
          return checkResult;
      }
     
-    public boolean checkDiagonalTopLeftToDownRight(Tile aTile) {
+    public static boolean checkDiagonalTopLeftToDownRight(int row, int col, Board aBoard) {
     	 //identify which color
-        int color = aTile.getColor();
+        int color = aBoard.getValue(row, col);
        //the xcoordinate and ycoordinate of tiles is the same as the index of their array index. 
-        int xIndex = aTile.getxCoord();
-        int yIndex = aTile.getyCoord();
         //count how many tiles in row
         int count=1;
         // check result
         boolean checkResult = false;
         
         //upleft
-        for(int i=1;(i<5) && (color==board[yIndex-i][xIndex-i].getColor() && board[yIndex-i][xIndex-i]!=null);i++) {
-            if(((yIndex-i)>=0) && (xIndex-i>=0)) {
+        for(int i=1;(i<5) && (color==aBoard.getValue(row-i, col-1));i++) {
+            if(((row-i)>=0) && (col-i>=0)) {
                 count++;
             }
         }
         //downright
-        for(int i=1;(i<5) && (color==board[yIndex+i][xIndex+i].getColor() && board[yIndex+i][xIndex+i]!=null);i++) {
-            if(((yIndex+i)<=15) && (xIndex+i<=15)) {
+        for(int i=1;(i<5) && (color==aBoard.getValue(row+i, col+1));i++) {
+            if(((row+i)<=15) && (col+i<=15)) {
                  count++;
             } 
         }
@@ -152,10 +122,5 @@ public class GameConfiguration extends Tile {
         } 
         return checkResult;
     } 
-        
-        
-	public static void main(String[] args) {
-		Board();
-	}
 
 }
