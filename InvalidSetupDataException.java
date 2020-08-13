f7c8c7e8a343fbb46aa6e8fa4915011373e05069
kennethl778 <67394621+kennethl778@users.noreply.github.com>
2020-08-13 14:06:48 -0600

    Angus' part for mission 3

---
+++ InvalidSetupDataException.java
@@ -0,0 +1,16 @@
+import java.lang.Exception;
+public class InvalidSetupDataException extends Exception{
+
+	public InvalidSetupDataException() {
+		
+	}
+	public InvalidSetupDataException(String msg) {
+		super(msg);
+	}
+	public InvalidSetupDataException(Throwable cause) {
+		super(cause);
+	}
+	public InvalidSetupDataException(String msg, Throwable cause) {
+		super(msg,cause);
+	}
+}
